72c4a1fc81cfd92e79cc589a9b5ba9b47e07a402
SunnyMinh <131391897+SunnyMinh@users.noreply.github.com>
2023-11-29 20:15:24 +0700

    Làm report

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","mydb") or die("Couldn't connect");
+$con = mysqli_connect("localhost", "root", "", "mydb") or die("Couldn't connect");
--- report.php
+++ report.php
@@ -339,51 +339,52 @@
                         
                         <?php
                         // Handle form submission
-                        // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         //     // Get search term and search type
-                        //     $searchTerm = $_POST['search'];
+                             $searchTerm = $_POST['search'];
 
-                        //     $sql = "SELECT TR.*, P.fName, P.lName
-                        //             FROM testingRecord TR
-                        //             INNER JOIN patient P ON TR.patientId = P.uniqueId
-                        //             WHERE TR.patientId ='$searchTerm'";
-                        //     $result = mysqli_query($con, $sql);
-                        //     // Display filtered patients
-                        //     echo '<h3>Search Results:</h3>';
-                        //     if ($result->num_rows === 0) {
-                        //         echo '<p>No matching patients found.</p>';
-                        //     } else {
-                        //         echo '<table border="1">';
-                        //         echo '<tr>';
-                        //         echo '<th>Name</th>';
-                        //         echo '<th>Test Number</th>';
-                        //         echo '<th>Test Date</th>';
-                        //         echo '<th>PCR Result</th>';
-                        //         echo '<th>PCR Cycle</th>';
-                        //         echo '<th>Quick Test Result</th>';
-                        //         echo '<th>Quick Test Cycle</th>';
-                        //         echo '<th>SPO2 Test Result</th>';
-                        //         echo '<th>Respiratory</th>';
-                        //         echo '</tr>';
+                            $sql = "SELECT P.fName, P.lName, P.gender, P.comorbitidies, TR.testType, TR.result, TR.testvalue, S.symptomName, T.result AS treatmentResult
+                                    FROM patient P
+                                    JOIN testingRecord TR ON TR.patientId = P.uniqueId
+                                    JOIN symptompatient SP ON SP.patientNumber = P.uniqueId
+                                    JOIN symptom S ON SP.symptomId = S.id
+                                    WHERE TR.patientId ='$searchTerm'";
+                            $result = mysqli_query($con, $sql);
+                            
+                             // Display filtered patients
+                            echo '<h3>Search Results:</h3>';
+                            if ($result->num_rows === 0) {
+                                 echo '<p>No matching patients found.</p>';
+                             } else {
+                                 echo '<table border="1">';
+                                 echo '<tr>';
+                                 echo '<th>Name</th>';
+                                 echo '<th>gender</th>';                                 
+                                 echo '<th>comorbitidies</th>';
+                                 echo '<th>testType</th>';
+                                 echo '<th>Test Result</th>';
+                                 echo '<th>Test Value</th>';
+                                 echo '<th>Symptoms</th>';
+                                 echo '<th>Treatment</th>';
+                                 echo '</tr>';
                         
-                        //         while ($row = $result->fetch_assoc()) {
-                        //             echo '<tr>';
-                        //             echo '<td>' . $row['fName'] . ' ' . $row['lName'] . '</td>';
-                        //             echo '<td>' . $row['testNumber'] . '</td>';
-                        //             echo '<td>' . $row['testDate'] . '</td>';
-                        //             echo '<td>' . $row['resultPCR'] . '</td>';
-                        //             echo '<td>' . $row['cyclePCR'] . '</td>';
-                        //             echo '<td>' . $row['resultQuick'] . '</td>';
-                        //             echo '<td>' . $row['cycleQuick'] . '</td>';
-                        //             echo '<td>' . $row['spO2'] . '</td>';
-                        //             echo '<td>' . $row['respiratory'] . '</td>';
-                        //             echo '</tr>';
-                        //         }
+                                 while ($row = $result->fetch_assoc()) {
+                                     echo '<tr>';
+                                     echo '<td>' . $row['fName'] . ' ' . $row['lName'] . '</td>';
+                                    echo '<td>' . $row['gender'] . '</td>';
+                                     echo '<td>' . $row['comorbitidies'] . '</td>';
+                                     echo '<td>' . $row['testType'] . '</td>';
+                                     echo '<td>' . $row['result'] . '</td>';
+                                     echo '<td>' . $row['testValue'] . '</td>';
+                                     echo '<td>' . $row['symptomName'] . '</td>';
+                                     echo '<td>' . $row['treatmentResult'] . '</td>';
+                                     echo '</tr>';
+                                 }
                         
-                        //         echo '</table>';
-                        //     }
+                                 echo '</table>';
+                             }
                     
-                        // }
+                         }
                         
                     ?>
                     
